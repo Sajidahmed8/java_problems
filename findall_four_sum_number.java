47ac676a9f871f7d2517bece8b7fb12540980ad8
Sajid Ahmed <ahmedsajid0709@gmail.com>
2023-09-26 22:33:31 +0530

    Problem-12

---
+++ findall_four_sum_number.java
@@ -0,0 +1,24 @@
+/**
+Problem : 12
+
+    Given an array A of integers and another number K. Find all the unique quadruple from the given array that sums up to K.
+    Also note that all the quadruples which you return should be internally sorted, i.e for any quadruple [q1,q2,q3,q4] the following should folloes:
+    q1<=q2<=q3<=q4.
+
+    Example :
+        Input :
+            N = 5, k = 3
+            A[] = {0,0,2,1,1}
+        Output :
+            0 0 1 2
+        Explanation : Sum of 0, 0, 1, 2 is equal to K.
+
+        Constraints :
+            1<=N<=100
+            -1000<=K<=1000
+            -100<=A[]<=100
+
+**/
+
+// Solution :-
+
